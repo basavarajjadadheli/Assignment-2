369d9e077ad2c8fe2387682426279d356fabd9e4
Basavaraj Neelappa Jadadheli <basavaraj.n@terrapay.com>
2022-08-03 16:50:58 +0530

    Assignment 2
    Signed-off-by: Basavaraj Neelappa Jadadheli <basavaraj.n@terrapay.com>

---
+++ Prdouct/src/main/java/com/product/entity/Product.java
@@ -0,0 +1,89 @@
+package com.product.entity;
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	@NotNull
+	private String name;
+	@NotNull
+	private String bigImage;
+	@NotNull
+	private String thumbnail;
+	@NotNull
+	private String discription;
+	@NotNull
+	private String shortDiscription;
+	private int ratings;
+	@NotNull
+	private float price;
+	@NotNull
+	private boolean isActive;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getBigImage() {
+		return bigImage;
+	}
+	public void setBigImage(String bigImage) {
+		this.bigImage = bigImage;
+	}
+	public String getThumbnail() {
+		return thumbnail;
+	}
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+	public String getDiscription() {
+		return discription;
+	}
+	public void setDiscription(String discription) {
+		this.discription = discription;
+	}
+	public String getShortDiscription() {
+		return shortDiscription;
+	}
+	public void setShortDiscription(String shortDiscription) {
+		this.shortDiscription = shortDiscription;
+	}
+	public int getRatings() {
+		return ratings;
+	}
+	public void setRatings(int ratings) {
+		this.ratings = ratings;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public boolean isActive() {
+		return isActive;
+	}
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	
+	
+	
+}
